8ce23b2a53a2f92d1c83e903cb9c940fc0f071cf
Samlittle24 <46633712+Samlittle24@users.noreply.github.com>
2019-03-31 17:00:19 -0400

    Add files via upload

---
+++ DestroyByBoundary.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByBoundary : MonoBehaviour
+{
+    private void OnTriggerExit(Collider other) 
+    {
+        Destroy(other.gameObject);
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
